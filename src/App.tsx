3353ca570813aca33628739a16c82cdd61ad5d35
Cyclone1070 <128883620+Cyclone1070@users.noreply.github.com>
2025-07-10 20:17:19 +1000

    added loading screen and assets fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { AnimatePresence, motion } from "motion/react";
-import { useRef, useState } from "react";
-import { HighlightButton } from "./components/HighlightButton";
+import { useEffect, useRef, useState } from "react";
 import { BgImage } from "./components/BgImage";
+import { HighlightButton } from "./components/HighlightButton";
 import { MainMenu } from "./components/MainMenu";
 import { NarrativeBox } from "./components/NarrativeBox";
 import { PlayerActionButtons } from "./components/PlayerActionButtons";
 import { PlayerActionTextArea } from "./components/PlayerActionTextArea";
 import type { Action } from "./type";
+import miracleSvgURL from "/miracle.svg?url";
 
 function App() {
 	const [directoryHandle, setDirectoryHandle] =
@@ -29,6 +30,48 @@ function App() {
 	};
 	// Refs for narrativeBg calculation
 	const actionButtonRef = useRef<HTMLButtonElement>(null);
+
+	// Assets loading screen
+	const [assetsLoaded, setAssetsLoaded] = useState(false);
+	useEffect(() => {
+		const loadVideo = new Promise((resolve) => {
+			const video = document.createElement("video");
+			// Check if the browser can play webm. If not, fall back to mp4.
+			const videoSrc = video.canPlayType("video/webm")
+				? "/splash-screen.webm"
+				: "/splash-screen.mp4";
+			video.src = videoSrc;
+			video.oncanplaythrough = resolve;
+			video.onerror = resolve;
+		});
+		loadVideo.then(() => {
+			setAssetsLoaded(true);
+		});
+	}, []);
+
+	if (!assetsLoaded) {
+		return (
+			<div className="w-screen h-screen flex flex-col gap-5 items-center justify-center text-white">
+				<motion.img
+					src={miracleSvgURL}
+					alt="Loading icon"
+					className="h-12 w-12"
+					animate={{
+						rotate: [0, 180, 180, 180],
+						scale: [1, 1, 0.5, 1],
+					}}
+					transition={{
+						duration: 2,
+						ease: "easeInOut",
+						times: [0, 0.5, 0.75, 1],
+						repeat: Infinity,
+						repeatDelay: 0.2,
+					}}
+				/>
+				Loading assets...
+			</div>
+		);
+	}
 
 	return (
 		<>
